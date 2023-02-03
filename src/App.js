0a2cedcb8f2721613776f2d899cd38f13a072b04
ByulKim <byulkim1225@gmail.com>
2023-02-03 22:26:53 +0900

    styled-components attrs추가 및 태그 변경

--- src/App.js
+++ src/App.js
@@ -12,22 +12,40 @@ const Box = styled.div`
 const Circle = styled(Box)`
   border-radius: 50px;
 `
+const Btn = styled.button`
+  color: #fff;
+  background-color: orange;
+  border: 0;
+  border-radius: 15px;
+`
 
 
 const Text = styled.span`
   color: #fff;
 `
 
+const Input = styled.input.attrs({required: true, minLength: 10 })`
+  background-color: red;
+`
+
 function App() {
   return (
-    <Father>
-      <Box bgColor="yellow">
-        <Text>123</Text>
-      </Box>
-      <Circle bgColor="green">
-        <Text>123</Text>
-      </Circle>
-    </Father>
+    <>
+      <Father as="header">
+        <Box bgColor="yellow">
+          <Text>123</Text>
+        </Box>
+        <Circle bgColor="green">
+          <Text>123</Text>
+        </Circle>
+        <Btn>Login</Btn>
+        <Btn as="a">LinkLogin</Btn>
+      </Father>
+      <Input/>
+      <Input/>
+      <Input/>
+    </>
+    
   );
 }
 
