0ae9944a79aed91c6684df50d7534230284c38af
ByulKim <byulkim1225@gmail.com>
2023-02-03 22:12:14 +0900

    styled-components 상속

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+
+const Father = styled.div`
+  display: flex;
+
+`
+const Box = styled.div`
+  width: 100px;
+  height: 100px;
+  background-color: ${(props) => props.bgColor};
+`
+const Circle = styled(Box)`
+  border-radius: 50px;
+`
+
+
+const Text = styled.span`
+  color: #fff;
+`
 
 function App() {
   return (
-    null
+    <Father>
+      <Box bgColor="yellow">
+        <Text>123</Text>
+      </Box>
+      <Circle bgColor="green">
+        <Text>123</Text>
+      </Circle>
+    </Father>
   );
 }
 
