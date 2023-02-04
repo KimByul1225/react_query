8ba568dc3d8e8d7e9739045682f473a141815e33
ByulKim <byulkim1225@gmail.com>
2023-02-04 13:29:34 +0900

    theme provider적용

--- src/App.js
+++ src/App.js
@@ -1,76 +1,27 @@
-import styled, {keyframes} from "styled-components";
+import styled from "styled-components";
 
-const Father = styled.div`
+const Title = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
+
+const Wrapper = styled.div`
   display: flex;
-
-`
-// const Box = styled.div`
-//   width: 100px;
-//   height: 100px;
-//   background-color: ${(props) => props.bgColor};
-// `
-// const Circle = styled(Box)`
-//   border-radius: 50px;
-// `
-// const Btn = styled.button`
-//   color: #fff;
-//   background-color: orange;
-//   border: 0;
-//   border-radius: 15px;
-// `
+  height: 100vh;
+  width: 100vw;
+  justify-content: center;
+  align-items: center;
+  background-color: ${(props) => props.theme.backgroundColor};
+`;
 
 
-// const Text = styled.span`
-//   color: #fff;
-// `
-
-// const Input = styled.input.attrs({required: true, minLength: 10 })`
-//   background-color: red;
-// `
-
-const ratationAnimation = keyframes`
-  0%{
-    transform: rotate(0deg);
-    border-radius: 0px;
-  }
-  50%{
-    transform: rotate(360deg);  
-    border-radius: 50px;
-
-  }
-  100%{
-    transform: rotate(0deg);
-    border-radius: 0px;
-  }
-`
-
-const Box = styled.div`
-  width: 100px;
-  height: 100px;
-  background-color: red;
-  animation: ${ratationAnimation} 2s linear infinite;
-`
 
 function App() {
   return (
     <>
-      {/* <Father as="header">
-        <Box bgColor="yellow">
-          <Text>123</Text>
-        </Box>
-        <Circle bgColor="green">
-          <Text>123</Text>
-        </Circle>
-        <Btn>Login</Btn>
-        <Btn as="a">LinkLogin</Btn>
-      </Father>
-      <Input/>
-      <Input/>
-      <Input/> */}
-      <Father>
-        <Box/>
-
-      </Father>
+      
+      <Wrapper>
+        <Title>Hello</Title>
+      </Wrapper>
     </>
     
   );
