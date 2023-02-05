777df67a11580bf102735cb97e44846068a2fe0a
ByulKim <byulkim1225@gmail.com>
2023-02-05 15:44:57 +0900

    event type지정

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
-import Circle from "./Circle";
+import React, { useState } from "react";
 
 function App() {
+  const [name, setName] = useState("")
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    console.log(event.currentTarget.value)
+    const {currentTarget: {value} } = event;
+    setName(value)
+  }
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) =>{
+    event.preventDefault();
+    console.log("hello", name)
+  }
   return (
     <div>
-      <Circle
-        bgColor="red"
-        borderColor="yellow"
-      />
-      <Circle
-        bgColor="green"
-        text="텍스트 텍스트"
-      />
+      <form onSubmit={onSubmit}>
+        <input value={name} onChange={onChange} type="text" placeholder="Name"/>
+        <button>로그인</button>
+      </form>
     </div>
   );
 }
