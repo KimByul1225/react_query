92cb20cde9ce44cb8067adae18b29a1358512b4c
ByulKim <byulkim1225@gmail.com>
2023-02-05 19:35:14 +0900

    코인 api연동

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 const Container = styled.div`
     padding: 0px 20px;
+    max-width: 480px;
+    margin: 0 auto;
 `
 const Header = styled.header`
     height: 10vh;
@@ -24,6 +26,8 @@ const Coin = styled.li`
         transition: color .2s ease-in;
         display: block;
         padding: 20px;
+        display: flex;
+        align-items: center;
 
     }
     &:hover{
@@ -38,51 +42,57 @@ const Title = styled.h1`
     font-size: 48px;
 `
 
-const coins = [
-    {
-        id: "btc-bitcoin",
-        name: "Bitcoin",
-        symbol: "BTC",
-        rank: 1,
-        is_new: false,
-        is_active: true,
-        type: "coin",
-    },
-    {
-        id: "eth-ethereum",
-        name: "Ethereum",
-        symbol: "ETH",
-        rank: 2,
-        is_new: false,
-        is_active: true,
-        type: "coin",
-    },
-    {
-        id: "hex-hex",
-        name: "HEX",
-        symbol: "HEX",
-        rank: 3,
-        is_new: false,
-        is_active: true,
-        type: "token",
-    },
-]
+const Loader = styled.div`
+    text-align: center;
+`
+
+const Img = styled.img`
+    width: 35px;
+    height: 35px;
+    margin-right: 10px;
+`
+
+interface CoinInterface {
+    id: string,
+    name: string,
+    symbol: string,
+    rank: number,
+    is_new: boolean,
+    is_active: boolean,
+    type: string
+}
 
 function Coins() {
+    const [coins, setCoins] = useState<CoinInterface[]>([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+
+        (async () => {
+            const response = await fetch("https://api.coinpaprika.com/v1/coins");
+            const json = await response.json();
+            console.log(json.slice(0, 100));
+            setCoins(json.slice(0, 100));
+            setLoading(false);
+        })();
+
+    },[])
+
     return (
         <Container>
             <Header>
                 <Title>코인</Title>
             </Header>
-            <CoinList>
+            {loading ? <Loader>Loading...</Loader> : <CoinList>
                 {coins.map(coin => (
                     <Coin key={coin.id}>
                         <Link to={`/${coin.id}`}>
+                            <Img src={`https://coinicons-api.vercel.app/api/icon/${coin.symbol.toLowerCase()}`} alt={coin.name} />
                             {coin.name} &rarr;
                         </Link>
                     </Coin>
                 ))}
-            </CoinList>
+            </CoinList>}
         </Container>
     );
 }
